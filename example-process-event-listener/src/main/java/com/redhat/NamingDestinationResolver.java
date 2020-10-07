645a0eae8965457fdb870eefee3cbccf8d028c17
Colum Foskin <cfoskin@redhat.com>
2020-10-07 11:54:30 +0100

    final

---
+++ example-process-event-listener/src/main/java/com/redhat/NamingDestinationResolver.java
@@ -0,0 +1,30 @@
+package com.redhat;
+
+import org.springframework.jms.support.destination.DestinationResolver;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+public class NamingDestinationResolver implements DestinationResolver {
+
+    private String destinationType;
+
+    private static final String TOPIC = "topic";
+    private static final String QUEUE = "queue";
+
+    public NamingDestinationResolver(String destinationType) {
+        this.destinationType = destinationType;
+    }
+
+    @Override
+    public Destination resolveDestinationName(Session session, String destinationName, boolean pubsub)
+            throws JMSException {
+        if (this.destinationType.toLowerCase().equals(TOPIC)) {
+
+            return session.createTopic(destinationName);
+        } else {
+            return session.createQueue(destinationName);
+        }
+    }
+}
